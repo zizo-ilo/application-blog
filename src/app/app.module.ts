1f0b324683ac3df66e3a759d7c053eec95feb81e
zizo-ilo <49061941+zizo-ilo@users.noreply.github.com>
2019-03-29 12:54:43 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BlogComponent } from './blog/blog.component';
 import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent,
     PostListComponent
   ],
   imports: [
